82d034cda4ee80e279c80c2b8badfecd3dd4da07
Sri Ganesh <sri.turner98@gmail.com>
2017-08-21 15:50:16 +0530

    Consolidating Javascript files
    sepearte files.

---
+++ backend-web/main.js
@@ -0,0 +1,41 @@
+var config = {
+  apiKey: "AIzaSyACMQ-svYix_njUCsVpy8k9zq_2ILamEGc",
+  authDomain: "fireit-2d2fb.firebaseapp.com",
+  databaseURL: "https://fireit-2d2fb.firebaseio.com",
+  projectId: "fireit-2d2fb",
+  storageBucket: "fireit-2d2fb.appspot.com",
+  messagingSenderId: "222536224576"
+};
+firebase.initializeApp(config);
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      document.querySelector('#login').addEventListener('click',function(){
+        var href='landing.html';
+        window.open(href);
+      });
+console.log("user is logged in")
+
+}
+
+  });
+  document.querySelector('#login').addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var email = document.querySelector('#email').value;
+    var password = document.querySelector('#password').value
+    var credential = firebase.auth.EmailAuthProvider.credential(email, password);
+    var auth = firebase.auth();
+    var currentUser = auth.currentUser;
+
+firebase.auth().signInWithEmailAndPassword(email,password)
+.catch(function (err) {
+ console.log("error")
+
+});
+  });
+    document.querySelector('#signout').addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    firebase.auth().signOut();
+  });
